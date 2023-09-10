3f828da9dd0c38d2a1aca9ac86902632322a8570
Simon Abello <abello.simon@gmail.com>
2023-09-10 09:16:03 +0200

    Created flask app, added home page, styles and fav

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return render_template('home.html')
+
+#DONE: Learn to change favicon icon
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
