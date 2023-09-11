5c77082064d0779dffe249a955cd40a522bddcfa
Simon Abello <abello.simon@gmail.com>
2023-09-12 00:24:29 +0200

    Fixed the bug with the templates. was endblock

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def resume():
 @app.route("/about")
 def about():
   return render_template('about.html')
-#DONE: Learn to change favicon icon
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
